431aeb34c4534ea5cead667702ef231eef3eb670
DELL <DELL@192.168.56.1>
2023-05-11 18:47:30 -0400

    Initial commit

---
+++ src/bonjourtoutlemonde/BonjourToutLeMonde.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonjourtoutlemonde;
+
+/**
+ *
+ * @author DELL
+ */
+public class BonjourToutLeMonde {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.printf("hello %s %s",args[0],args[1]);
+    }
+    
+}
